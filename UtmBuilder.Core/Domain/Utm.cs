515b97db319375c3f745872606a8a3c49ad3b450
PedroAbreuHS <pedroabreu.hs@gmail.com>
2024-06-10 14:36:02 -0300

    teste

---
+++ UtmBuilder.Core/Domain/Utm.cs
@@ -0,0 +1,11 @@
+﻿using UtmBuilder.Core.ValueObjects;
+
+namespace UtmBuilder.Core.Domain
+{
+    public class Utm
+    {
+        public Url Url { get; set; } = null!;
+
+        public Campaign Campaign { get; set; } = null!;
+    }
+}
---
+++ UtmBuilder.Core/ValueObjects/Campaign.cs
@@ -0,0 +1,22 @@
+﻿namespace UtmBuilder.Core.ValueObjects
+{
+    public class Campaign : ValueObject
+    {
+        public string Source { get; set; }
+        public string Medium { get; set; }
+        public string Name { get; set; }
+        public string? Id { get; set; }
+        public string? Term { get; set; }
+        public string? Content { get; set; }
+
+        public Campaign(string source, string medium, string name, string? id = null, string? term = null, string? content = null)
+        {
+            Source = source;
+            Medium = medium;
+            Name = name;
+            Id = id;
+            Term = term;
+            Content = content;
+        }
+    }
+}
